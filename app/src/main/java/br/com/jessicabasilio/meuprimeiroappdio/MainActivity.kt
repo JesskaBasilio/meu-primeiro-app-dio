ba60f5b013c7eef76d390d0ef7f321a4fe72d003
Jessica Basilio <jessica.limabasilio@gmail.com>
2025-01-26 09:54:29 -0300

    Commit Desafio alunos do curso Entendendo a Estrutura e Depurando Apps Android

---
+++ app/src/main/java/br/com/jessicabasilio/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.jessicabasilio.meuprimeiroappdio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.jessicabasilio.meuprimeiroappdio.ui.theme.MeuPrimeiroAppDioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeuPrimeiroAppDioTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Jessica",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MeuPrimeiroAppDioTheme {
+        Greeting("Jessica")
+    }
+}
